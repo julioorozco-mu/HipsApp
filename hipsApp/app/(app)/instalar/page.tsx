f3c3b0a65a32dcd965c57ff25fd863febb1a5540
Julio Orozco <julio.orozco@unach.mx>
2026-08-03 16:00:01 -0600

    Elimina pantalla PWA personalizada

--- hipsApp/app/(app)/instalar/page.tsx
+++
@@ -1,6 +0,0 @@
-import { InstallPwaCard } from "@/components/features/more/install-pwa-card";
-import { MoreShell } from "@/components/features/more/more-shell";
-
-export default function InstallPage() {
-  return <MoreShell title="Instalar HipsApp"><InstallPwaCard /></MoreShell>;
-}
